cbfe7496347cac3086116e8e0142b8d94e7e38b4
madhushree <kri.madhushree93@gmail.com>
2020-03-22 23:14:56 +0530

    Submit button adding a new row

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Table from './Table';
 
+
 class Home extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            newEmployeeName: "",
           employees : [
             {id: 1, name: 'Madhushree'},
             {id: 2, name: 'Priyanka'},
@@ -15,11 +17,37 @@ class Home extends React.Component {
         }
     }
 
+    validParams(){
+        if(this.state.newEmployeeName != undefined && this.state.newEmployeeName != "") {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    triggerSubmit(){
+        if (this.validParams()){
+            var listOfEmployee = this.state.employees
+            var newEmployee = {
+                id: listOfEmployee.length + 1,
+                name: this.state.newEmployeeName
+            }
+            listOfEmployee.push(newEmployee)
+            this.setState({
+                employees: listOfEmployee
+            })
+        }
+    }
+
     render() {
         return (
             <div className="Search" align="center">
-                <input type="text" placeholder="Search"/>
-                <button> Submit </button>
+                <input type="text" placeholder="Search" onChange={(e) => {
+                  this.setState({
+                      newEmployeeName: e.target.value
+                  })  
+                }}/>
+                <button onClick={this.triggerSubmit.bind(this)}> Submit </button>
                 <br /><br />
                 <Table employees={this.state.employees} />
             </div>
